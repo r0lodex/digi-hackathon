a2beb88b2905b3dfa3ace61b9dda11a4c9bd32a5
Irfan Radzi <rolodex888+rssb@gmail.com>
2017-07-30 02:51:09 +0800

    Gulp prep for dashboard

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,31 @@
 /* jshint ignore:start */
 // DEPENDENCIES
 // ========================================
-var gulp          = require('gulp'),
-    sass          = require('gulp-sass'),
-    prefix        = require('gulp-autoprefixer'),
-    uglify        = require('gulp-uglify'),
-    rename        = require('gulp-rename'),
-    concat        = require('gulp-concat'),
-    notify        = require('gulp-notify'),
-    include       = require('gulp-include'),
-    gutil         = require('gulp-util');
+var gulp     = require('gulp'),
+    sass     = require('gulp-sass'),
+    prefix   = require('gulp-autoprefixer'),
+    uglify   = require('gulp-uglify'),
+    rename   = require('gulp-rename'),
+    concat   = require('gulp-concat'),
+    notify   = require('gulp-notify'),
+    include  = require('gulp-include'),
+    gutil    = require('gulp-util');
 
 gulp.task('minify-js', consolidateJSFiles);
 gulp.task('minify-css', minifycss);
+
+gulp.task('minify-dash-js', dashboardCompileJS);
+gulp.task('minify-dash-css', minifyDashboardCss);
+
 gulp.task('corejs', concatJS);
 
 gulp.task('default', function() {
     gulp.start(
         'minify-js',
         'minify-css',
+        'minify-dash-js',
+        'minify-dash-css',
+
         'corejs'
     )
 });
@@ -52,7 +59,23 @@ function consolidateJSFiles() {
     return gulp.src('./app/**/*.js')
         .pipe(concat('app.js'))
         .pipe(rename({ suffix: '.min' }))
-        //.pipe(uglify().on('error', gutil.log))
         .pipe(gulp.dest('./dist/js'))
         .pipe(notify({ message: 'App JS minified' }))
+}
+
+function dashboardCompileJS() {
+    return gulp.src('./dashboard/**/*.js')
+        .pipe(concat('dashboard.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./dist/dashboard'))
+        .pipe(notify({ message: 'Dashboard JS minified' }))
+}
+
+function minifyDashboardCss() {
+    return gulp.src('./dashboard/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(concat('dashboard.css'))
+        .pipe(prefix())
+        .pipe(gulp.dest('./dist/dashboard'))
+        .pipe(notify({ message: 'Dashboard CSS minified.' }))
 }
