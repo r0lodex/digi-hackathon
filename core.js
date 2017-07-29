531126940d7f4a2e76e9879f84d4d423682c9e9f
Irfan Radzi <rolodex888+rssb@gmail.com>
2017-07-29 16:58:46 +0800

    Added some library + deps

---
+++ core.js
@@ -0,0 +1,7 @@
+//include "./node_modules/angular/angular.js"
+//include "./node_modules/angular-resource/angular-resource.js"
+//include "./node_modules/@uirouter/core/_bundles/ui-router-core.js"
+
+//include "./node_modules/jquery/dist/jquery.js"
+//include "./node_modules/tether/dist/js/tether.js"
+//include "./node_modules/bootstrap/dist/bootstrap.js"
